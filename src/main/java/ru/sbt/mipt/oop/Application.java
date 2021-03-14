f023ae61f8340c0cd4397a66c726e791f461e6d7
timurjava <timur_liza@mail.ru>
2021-03-14 15:52:47 +0300

    spring

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,6 +1,9 @@
 package ru.sbt.mipt.oop;
 
 import com.coolcompany.smarthome.events.SensorEventsManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import ru.sbt.mipt.oop.Alarm.SMSSender;
 import ru.sbt.mipt.oop.Alarm.Sender;
 import ru.sbt.mipt.oop.EventHandlers.*;
@@ -12,25 +15,8 @@ import java.util.*;
 public class Application {
 
     public static void main(String... args) throws IOException {
-        String path = "smart-home-1.js";
-        // считываем состояние дома из файла
-        JsonSmartHomeStateProvider json = new JsonSmartHomeStateProvider(path);
-        SmartHome smartHome = json.provideSmartHome();
-        Sender sender = new SMSSender();
-        List<EventHandlerMy> eventProcessors = new ArrayList<>(Arrays.asList(
-                new DoorEventHandlerMy(smartHome),
-                new LightEventHandlerMy(smartHome),
-                new HallDoorEventHandlerMy(smartHome)));
-        Map<String, States> testmap = new HashMap<>();
-        testmap.put("LightIsOn", States.LIGHT_ON);
-        testmap.put("LightIsOff", States.LIGHT_OFF);
-        testmap.put("DoorIsOpen", States.DOOR_OPEN);
-        testmap.put("DoorIsClosed", States.DOOR_CLOSED);
-        SensorEventsManager sensorEventsManager = new SensorEventsManager();
-        for (EventHandlerMy processor : eventProcessors) {
-            sensorEventsManager.registerEventHandler(
-                    new CoolCompanyAdapter(new SecurityProcessorDecorator(processor, sender, smartHome), testmap));
-        }
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        SensorEventsManager sensorEventsManager = context.getBean(SensorEventsManager.class);
         sensorEventsManager.start();
     }
 
